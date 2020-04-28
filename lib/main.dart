9afa0a632eaa1f98996d944c8b8ebabd48d932fb
Paulo Filho <pcf.feat@gmail.com>
2020-04-28 09:06:47 -0400

    teach: how to display a network image within a centered widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,12 @@ void main() {
           title: Text('I Am Rich'),
           backgroundColor: Colors.blueGrey[900],
         ),
+        body: Center(
+          child: Image(
+            image:
+                NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+          ),
+        ),
       ),
     ),
   );
