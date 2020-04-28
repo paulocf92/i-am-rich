f5dea9172116a2901fbd4c0d471ee581563258f1
Paulo Filho <pcf.feat@gmail.com>
2020-04-28 08:54:53 -0400

    teach: how to use a scaffold to wrap our app with a custom heads-up bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
       ),
     ),
   );
